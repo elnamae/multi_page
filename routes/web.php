e92eb67f6506224e7bd4a8f8e58baacb960650b4
elnamae <elnaebo@ckcm.edu.ph>
2024-07-23 16:16:30 +0800

    adding pages

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/pricing', function () {
+    return view('pricing');
+});
+
+Route::get('/FAQ', function () {
+    return view('FAQ');
+});
+
+Route::get('/blog-home', function () {
+    return view('blog-home');
+});
+
+Route::get('/blog-post', function () {
+    return view('blog-post');
 });
